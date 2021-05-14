10c82007bbb3f7972f9574e70866095b2a7cc20a
Thomas M <twsm000@gmail.com>
2021-05-14 20:14:56 -0300

    Project created

---
+++ backend/src/main/java/com/twsm/projectbds01crud/ProjectBds01CrudApplication.java
@@ -0,0 +1,13 @@
+package com.twsm.projectbds01crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBds01CrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectBds01CrudApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/twsm/projectbds01crud/ProjectBds01CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twsm.projectbds01crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBds01CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
