1d7bd27aa756db06ba7aae824fa8a2e78f005cd4
Thomas M <twsm000@gmail.com>
2021-05-15 01:36:58 -0300

    Client endpoint insert implemented

--- backend/src/main/java/com/twsm/projectbds01crud/resources/ClientResource.java
+++ backend/src/main/java/com/twsm/projectbds01crud/resources/ClientResource.java
@@ -9,9 +9,14 @@ import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/clients")
@@ -38,5 +43,15 @@ public class ClientResource {
     @GetMapping(value = "/{id}")
     public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
         return  ResponseEntity.ok(clientService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto) {
+        dto = clientService.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri()
+                    .path("/{id}")
+                    .buildAndExpand(dto.getId())
+                    .toUri();
+        return ResponseEntity.created(uri).body(dto);
     }
 }
--- backend/src/main/java/com/twsm/projectbds01crud/services/ClientService.java
+++ backend/src/main/java/com/twsm/projectbds01crud/services/ClientService.java
@@ -1,12 +1,14 @@
 package com.twsm.projectbds01crud.services;
 
 import com.twsm.projectbds01crud.dto.ClientDTO;
+import com.twsm.projectbds01crud.entities.Client;
 import com.twsm.projectbds01crud.repositories.ClientRepository;
 import com.twsm.projectbds01crud.services.exceptions.ResourceNotFound;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ClientService {
@@ -14,15 +16,32 @@ public class ClientService {
     @Autowired
     private ClientRepository clientRepository;
 
+    @Transactional(readOnly = true)
     public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
         return clientRepository.findAll(pageRequest)
                 .map(ClientDTO::new);
     }
 
-
+    @Transactional(readOnly = true)
     public ClientDTO findById(Long id) {
         return clientRepository.findById(id)
                 .map(ClientDTO::new)
                 .orElseThrow(() -> new ResourceNotFound("Entity not found"));
+    }
+
+    @Transactional
+    public ClientDTO insert(ClientDTO dto) {
+        Client entity = new Client();
+        copyDtoToEntity(dto, entity);
+        entity = clientRepository.save(entity);
+        return new ClientDTO(entity);
+    }
+
+    private void copyDtoToEntity(ClientDTO dto, Client entity) {
+        entity.setBirthDate(dto.getBirthDate());
+        entity.setChildren(dto.getChildren());
+        entity.setCpf(dto.getCpf());
+        entity.setIncome(dto.getIncome());
+        entity.setName(dto.getName());
     }
 }
